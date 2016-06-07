a29062cfe29a7615e82698fd0884474777344f05
Pawel Niewiadomski <11110000b@gmail.com>
2016-06-07 18:52:13 +0200

    fixed tests for configuration

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,7 +4,19 @@ describe 'configuration' do
 
   describe 'api_key' do
     it 'returns default key' do
-      expect(Promoter.api_key).to eq('ribeyeeulorem')
+      expect(promoter.api_key).to eq('ribeyeeulorem')
+    end
+
+    it 'cannot be changed' do
+      expect {
+        promoter.api_key = 'test'
+      }.to raise_error NoMethodError
+    end
+
+    it 'is not global' do
+      expect {
+        Promoter::api_key
+      }.to raise_error NoMethodError
     end
   end
 
